e1da61e225d5f9b08ece73cda192d4fac459a12c
Timon Lilje <timon.lilje@gmail.com>
2021-09-10 23:40:07 +0200

    hex color display

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let width = 400;
 let height = 400;
 
 function setup() {
-  createCanvas(width, height);
+  let canvas = createCanvas(width, height);
+  canvas.parent("canvas")
   colors[0] = new Color;
 }
 
@@ -15,4 +16,29 @@ function draw() {
     fill(colors[i].out())
     rect(i * scl, 0, i * scl + scl, height)
   }
+
+  hoverUpdateOutput()
 }
+
+let hoverUpdateOutput = () => {
+  let scl = width / colors.length;
+  let x = Math.floor(mouseX / scl);
+
+  if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
+    return false;
+  } else {
+
+    let c = colors[x]
+    document.getElementById("hexColor").innerHTML = convertColorToHex(c);
+    return true;
+  }
+}
+
+let convertColorToHex = c => {
+  let r = decToHex(c.r);
+  let g = decToHex(c.g);
+  let b = decToHex(c.b);
+  return `#${r}${g}${b}`;
+}
+
+let decToHex = x => x.toString(16).toUpperCase();
